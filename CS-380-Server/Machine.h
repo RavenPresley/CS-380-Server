3872ee6a27f03581287a5b85274607bd94232890
JonCJenkins <jcjenkins2@moreheadstate.edu>
2021-10-28 10:46:01 -0400

    Added private members to machine

--- CS-380-Server/Machine.h
+++ CS-380-Server/Machine.h
@@ -1,10 +1,15 @@
 #pragma once
 #include <iostream>
+#include<string>
 
 class Machine
 {
 public:
-
+	
 private:
-
+	std::string assetTag;
+	std::string serviceTagNum;
+	std::string makeAndModel;
+	std::string serialNum;
+	bool isMac;
 };
