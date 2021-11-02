f5f7e2f3f2a137bbdf61c42b70bcf09e541bf6f5
mykeltidw <mwheatley2@moreheadstate.edu>
2021-11-02 10:35:16 -0400

    added public and private members to IUMachine and OUMachine

--- CS-380-Server/IUMachine.h
+++ CS-380-Server/IUMachine.h
@@ -1,11 +1,20 @@
 #pragma once
+#include "Machine.h"
+#include <string>
 #include <iostream>
 
 
 class IUMachine : public Machine
 {
 public:
+	void MoveOutOfUse();
+	IUMachine(); //default contructor
 
 private:
-
+	std::string buildingName;
+	std::string roomNum;
+	std::string publicOrPrivate;
+	std::string departmentInfo;
+	std::string ownerInfo;
+	
 };
--- CS-380-Server/OUMachine.h
+++ CS-380-Server/OUMachine.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "Machine.h"
+#include <string>
+#include <iostream>
+
+class OUMachine : public Machine
+{
+public:
+	void MoveInUse();
+	OUMachine();	//default constructor
+
+private:
+	bool surplusStatus;
+	bool reimageStatus;
+	bool workingStatus;
+	std::string IT_Location;
+
+};
