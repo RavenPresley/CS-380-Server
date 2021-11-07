84a60b697941ad06380357625fbd42ef94824e3b
RavenPresley <ravenpatrick01@gmail.com>
2021-11-07 18:39:21 -0500

    Added definitions for LoginStorage
    Defined TryLogin() and

--- CS-380-Server/LoginStorage.h
+++ CS-380-Server/LoginStorage.h
@@ -1,21 +1,44 @@
-#pragma once
+##pragma once
 #include <iostream>
 
 class LoginStorage
 {
 public:
-	void TryLogin();
-	void CreateLogin();
+	bool TryLogin(std::string, std::string);
+	void CreateLogin(std::string, std::string);
+	
+private: 
+	std::string correctUserName;
+	std::string correctPasword;
 };
 
-
-
-void LoginStorage::TryLogin()
+bool LoginStorage::TryLogin(std::string userNameInput, std::string passwordInput)
 {
+	if (correctUserName == NULL)
+	{
+		std::cout << "There is no user name or password set up currently. Please set up a User Name and Password..." << endl;
+	}
+	else if ((userNameInput == correctUserName) && (passwordInput == correctPassword))
+	{
+		// Grant access to the registry
+	}
+	else
+	{
+		// Deny access to the registry - will need to attempt login again
 
+	}
 }
 
-void LoginStorage::CreateLogin()
+void LoginStorage::CreateLogin(std::string newUserName, std::string newPassword)
 {
+	// Set correctUserName = newUserName for TryLogin() to validate later
+	correctUserName = newUserName;
 
+	// Set correctPassword = newPassword for TryLogin() to validate later
+	correctPassword = newPassword;
+
+	/*
+	std::cout << "Your User Name and Password have been registered!" << endl;
+	std::cout << "You may now proceed to Login." << endl;
+	*/
 }
